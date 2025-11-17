8eb50d066a34eee26b419358dff5ebfede6b9103
Shashank Shukla <Shashankshukla192004@gmail>
2025-11-17 23:41:46 +0530

    Updated folder

--- Basics JAVA/First.java
+++ Basics JAVA/First.java
@@ -1,3 +1,20 @@
 public class First {
-          
+   public static void main(String[] args) {
+          System.out.println("Ja mata di"); // New line
+          System.out.print("This is first code of in java \n Shashank Shukla.!");  // New line
+          System.out.print("1 2 3 4 5");   // Same line
+
+
+          System.out.print("\n* * * * \n* * * \n* * \n* ");
+          System.out.println("\n* * * *");
+          System.out.println("* * *");
+          System.out.println("* *");
+          System.out.println("* ");
+
+
+          int a = 10;
+          int b = 15;
+          a = b;
+          System.out.print(a+b);
+}
 }
