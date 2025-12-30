859e538c1374b566ab979f84eb2bca685c7ee024
Shashank Shukla <Shashankshukla192004@gmail>
2025-12-30 23:09:27 +0530

    Java Series With Automation

--- Basics JAVA/loop.java
+++ Basics JAVA/loop.java
@@ -4,16 +4,45 @@ public class loop {
           public static void main(String[] args) {
                     
                     Scanner sc = new Scanner(System.in);
+          //! FOR LOOP
+
 
           //        for(int i = 1; i <= 10; i++){
           //           System.out.printf("Welcome to DRT %s \n", i );
           //        }
                  
-                 //~ Print a 2 table in java 
-                 for(int i = 1; i <= 10; i++){
-                    int mul = 2*i;
-                    System.out.printf(" 2 * %d = %d \n", i , mul);
-                 }
-          }
+          //        //~ Print a 2 table in java 
 
+                 int num = sc.nextInt(); 
+                 for(int i = 1; i <= 10; i++){
+                    int mul = num*i;
+                    System.out.printf(" %d * %d = %d \n",num, i , mul);
+                 }
+
+          //! While Loop
+          // int i = 1;
+          // while(i < 11){
+          //           System.out.println("Jai Mata di");
+          //           i++;
+          // }
+
+
+          //! Do While Loop
+          // int i = 1;
+          // do{
+          //           System.out.println("Do While is Running...");
+          //           i++;
+          //           System.out.println(i);
+          // }while(i < 10);
+          // }
+
+          //! NAtural Number sum
+          // int n = sc.nextInt();
+          // int sum = 0;
+          // for(int i = 0; i <= n; i++){
+          //           sum = sum + i;
+          // }
+          // System.out.println(sum);
+
+           }
 }
